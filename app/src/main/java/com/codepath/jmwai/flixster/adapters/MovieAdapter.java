2fb79b9a970357957e2e394fd4e92d720e274e27
John Mwai <jmwai@conncol.edu>
2020-10-02 03:06:14 -0400

    Flixster Part 2

--- app/src/main/java/com/codepath/jmwai/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/jmwai/flixster/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.codepath.jmwai.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.jmwai.flixster.DetailActivity;
 import com.codepath.jmwai.flixster.R;
 import com.codepath.jmwai.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -57,10 +63,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
+
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -68,9 +75,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
@@ -82,6 +90,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            //Register click listener on the whole row
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Navigate to a new activity on tap
+                    Intent i  = new Intent(context, DetailActivity.class);
+
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
